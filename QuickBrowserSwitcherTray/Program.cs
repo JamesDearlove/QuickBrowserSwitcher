d03591e00b392efc71a45b863797c86d3cd2b4f4
James Dearlove <jamesdearlove@outlook.com>
2021-12-07 09:48:23 +1000

    Rewriting half the tray app because it was terrible

--- QuickBrowserSwitcherTray/Program.cs
+++ QuickBrowserSwitcherTray/Program.cs
@@ -6,36 +6,43 @@ class Program
     const string RegistryKeyRoot = @"SOFTWARE\Quick Browser Switcher";
     const string SettingBrowserExec = "BrowserExec";
 
-    static string currentBrowser = "NOT LOADED";
-    static NotifyIcon trayIcon;
+    static NotifyIcon trayIcon = new();
+    static ToolStripMenuItem switchItem = new();
 
-    static void trayIcon_MouseClick(object Sender, MouseEventArgs e)
+    private static void trayIcon_MouseClick(object Sender, MouseEventArgs e)
     {
         if (e.Button == MouseButtons.Left)
         {
-            var regKey = Registry.CurrentUser.CreateSubKey(RegistryKeyRoot);
-            var browserKey = regKey.GetValue(SettingBrowserExec);
-
-            string setTo;
-            if (browserKey == null || (string)browserKey == "msedge.exe")
-            {
-                setTo = "firefox.exe";
-                trayIcon.Icon = Icon.ExtractAssociatedIcon(@"C:\Program Files\Mozilla Firefox\firefox.exe");
-            }
-            else
-            {
-                setTo = "msedge.exe";
-                trayIcon.Icon = Icon.ExtractAssociatedIcon(@"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe");
-            }
-
-            regKey.SetValue(SettingBrowserExec, setTo);
-            regKey.Close();
-
-            currentBrowser = setTo;
-            trayIcon.Text = $"Current Browser: {currentBrowser}";
-
-            Console.WriteLine($"Selected Browser: {setTo}");
+            switchBrowsers();
         }
+    }
+
+    private static void SwitchItem_Click(object? sender, EventArgs e)
+    {
+        switchBrowsers();
+    }
+
+    static void switchBrowsers()
+    {
+        var regKey = Registry.CurrentUser.CreateSubKey(RegistryKeyRoot);
+        var browserKey = regKey.GetValue(SettingBrowserExec);
+
+        string setTo;
+        if (browserKey == null || (string)browserKey == "msedge.exe")
+        {
+            setTo = "firefox.exe";
+        }
+        else
+        {
+            setTo = "msedge.exe";
+        }
+
+        regKey.SetValue(SettingBrowserExec, setTo);
+        regKey.Close();
+
+        updateTray(setTo);
+
+        Console.WriteLine($"Selected Browser: {setTo}");
     }
 
     static void contextQuit_Click(object Sender, EventArgs e)
@@ -46,35 +53,53 @@ class Program
     static void createTrayIcon()
     {
         trayIcon = new NotifyIcon();
-        trayIcon.Text = $"Current Browser: {currentBrowser}";
-        trayIcon.Icon = Icon.ExtractAssociatedIcon(@"C:\Program Files\Mozilla Firefox\firefox.exe");
-
         trayIcon.Visible = true;
         trayIcon.MouseClick += trayIcon_MouseClick;
 
+        updateTray();
+
         var contextMenu = new ContextMenuStrip();
+        contextMenu.Items.Add(switchItem);
+        switchItem.Click += SwitchItem_Click;
+
         contextMenu.Items.Add("Quit", null, contextQuit_Click);
+
         trayIcon.ContextMenuStrip = contextMenu;
     }
 
-    static void getCurrentBrowser()
+    static void updateTray()
     {
         var regKey = Registry.CurrentUser.CreateSubKey(RegistryKeyRoot);
         var browserKey = regKey.GetValue(SettingBrowserExec);
 
-        if (browserKey == null)
+        if (browserKey != null)
         {
-            currentBrowser = "Not set";
+            updateTray((string)browserKey);
         }
-        else
+
+    }
+
+    static void updateTray(string browser)
+    {
+        trayIcon.Text = $"Current Browser: {browser}";
+
+        switch (browser)
         {
-            currentBrowser = browserKey.ToString();
+            case "firefox.exe":
+                trayIcon.Icon = Icon.ExtractAssociatedIcon(@"C:\Program Files\Mozilla Firefox\firefox.exe");
+                switchItem.Text = "Switch to msedge.exe";
+                break;
+            case "msedge.exe":
+                trayIcon.Icon = Icon.ExtractAssociatedIcon(@"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe");
+                switchItem.Text = "Switch to firefox.exe";
+                break;
+            default:
+                break;
         }
     }
 
     public static void Main(string[] args)
     {
-        getCurrentBrowser();
         createTrayIcon();
         Console.WriteLine("Hello, World!");
         Application.Run();
